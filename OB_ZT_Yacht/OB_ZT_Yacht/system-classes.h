abe399978b2e9cf40edd01ae3090a01c7b64b440
dohyeon0929 <eythgj3589@gmail.com>
2022-12-05 01:51:13 +0900

    이거 받아봐

---
+++ OB_ZT_Yacht/OB_ZT_Yacht/system-classes.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "essential.h"
+class InputManager {
+private:
+public:
+	
+};
+
+class StartSceneInputManager : InputManager {
+
+};
+
+class GameSceneInputManager : InputManager {
+
+};
+
+class EndSceneInputMangaer :InputManager {
+
+};
+
+class GameProcess {
+
+};
+
+class GameStart{
+
+};
+
+class Round {
+
+};
+
+class Turn {
+
+};
+
+class Roll {
+
+};
