aff5ccbcf3bc9b355a8fbf80bb5ebad47facfda9
dohyeon0929 <eythgj3589@gmail.com>
2022-12-04 17:26:47 +0900

    개요 : 주사위 관련 클래스들 구현 완료
    상세:
    - Dice : 주사위 자체에 관련한 클래스 인터페이스
        ㄴNormalDice : 평범한 1,2,3,4,5,6 주사위
        ㄴTwoSideDice : 2가지 숫자로만 이루어진 주사위. SelectNums 사용
    -SelectNums : n개의 숫자를 고르게 한다
    -DiceSet : 주사위 집합. vector<Dice> 기반
        ㄴActivateDiceSet : 활성화 상태인 주사위 집합. multiset<Dice> 기반
    -DiceState : 주사위의 상태를 나타내는 인터페이스
        ㄴDiceStateActivated : 다음 roll때 굴릴 수 있는 주사위
        ㄴDiceStateKeep : 다음 roll때 굴리지 않는 주사위
    -DiceSetFactory : DiceSet을 생성하는 클래스. Dice개수와 여러 특수 다이스를 전달받을 수 있다.

--- OB_ZT_Yacht/OB_ZT_Yacht/essential.h
+++ OB_ZT_Yacht/OB_ZT_Yacht/essential.h
@@ -10,10 +10,4 @@
 #include <time.h>
 #include <random>
 using namespace std;
-int RandRange(int min, int max)
-{
-    static const double fraction = 1.0 / (RAND_MAX + 1.0);  // static used for efficiency, so we only calculate this value once
-    // evenly distribute the random number across our range
-    return min + static_cast<int>((max - min + 1) * (std::rand() * fraction));
-}
 #endif
