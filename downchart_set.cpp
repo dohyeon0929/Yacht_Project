3e417d1e258c5adc627c547d1399ffaaca6e6e56
sweetholly <80771455+sweetholly@users.noreply.github.com>
2022-12-06 19:00:18 +0900

    Update downchart_set.cpp

--- downchart_set.cpp
+++ downchart_set.cpp
@@ -2,21 +2,7 @@
 
 using namespace std;
 
-bool DownChart::IsNullptr(int* value)
-{
-	int result = 0;
-	if (*value)
-	{
-		return false;
-	}
-	else return true;
-}
-int DownChart::SetValue(vector<int>dice_num)
-{
-	return 0;
-}
-
-//FOK °ª ÀÖ´ÂÁö È®ÀÎ, ´ëÀÔ
+//FOK ê°’ ìˆëŠ”ì§€ í™•ì¸, ëŒ€ì…
 int CheckFOK::SetValue(vector<int>dice_num)
 {
 	int result = 0;
@@ -32,8 +18,106 @@ int CheckFOK::SetValue(vector<int>dice_num)
 	}
 	return result;
 }
+CheckFOK::CheckFOK(Chart* chart)
+{
+	this->chart = chart;
+}
 
-//Full House °ª ÀÖ´ÂÁö È®ÀÎ, ´ëÀÔ
+//Full House ê°’ ìˆëŠ”ì§€ í™•ì¸, ëŒ€ì…
+int CheckFH::SetValue(vector<int>dice_num)
+{
+	int result = 0;
+	int check_value[6] = { 0, };
+	for (int i = 0; i < dice_num.size(); i++)
+	{
+		check_value[dice_num[i]] += 1;
+	}
+	for (int i = 0; i < dice_num.size(); i++)
+	{
+		if ((check_value[i] == 3))
+			for (int j = 0; j < 5; j++)
+			{
+				if (i == j) continue;
+				if (check_value[j] == 3) {
+					result = (i + 1) * 3 + (j + 1) * 2;
+					break;
+				}
+			}
+	}
+	return result;
+}
+CheckFH::CheckFH(Chart* chart)
+{
+	this->chart = chart;
+}
+
+
+//Small Straight ê°’ ìˆëŠ”ì§€ í™•ì¸, ëŒ€ì…
+int CheckSS::SetValue(vector<int>dice_num)
+{
+	int result = 0;
+	if ((accumulate(dice_num.begin(), dice_num.begin() + 3, 0) == 14 && dice_num.front() == 1) ||
+		(accumulate(dice_num.begin() + 1, dice_num.end(), 0) == 14 && dice_num.back() == 4))
+		result = 30;
+
+	else if ((accumulate(dice_num.begin(), dice_num.begin() + 3, 0) == 14 && dice_num.front() == 2) ||
+		(accumulate(dice_num.begin() + 1, dice_num.end(), 0) == 14 && dice_num.back() == 5))
+		result = 30;
+	else if ((accumulate(dice_num.begin(), dice_num.begin() + 3, 0) == 18 && dice_num.front() == 3) ||
+		(accumulate(dice_num.begin() + 1, dice_num.end(), 0) == 18 && dice_num.back() == 6))
+		result = 30;
+
+	return result;
+}
+CheckSS::CheckSS(Chart* chart)
+{
+	this->chart = chart;
+}
+
+
+//Big Straight ê°’ ìˆëŠ”ì§€ í™•ì¸, ëŒ€ì…
+int CheckBS::SetValue(vector<int>dice_num)
+{
+	int result = 0;
+	if ((accumulate(dice_num.begin(), dice_num.end(), 0) == 15 && dice_num.front() == 1 &&dice_num.back() == 5)) 
+		result = 40;
+	if ((accumulate(dice_num.begin(), dice_num.end(), 0) == 20 && dice_num.front() == 2 && dice_num.back() == 6))
+		result = 40;
+	return result;
+}
+CheckBS::CheckBS(Chart* chart)
+{
+	this->chart = chart;
+}
+
+
+//yacht ê°’ ìˆëŠ”ì§€ í™•ì¸, ëŒ€ì…
+int CheckYacht::SetValue(vector<int>dice_num)
+{
+	int result = 50;
+	if (dice_num[0] == dice_num[1] == dice_num[2] == dice_num[3] == dice_num[4])
+		return 50;
+	else return 0;
+}
+CheckYacht::CheckYacht(Chart* chart)
+{
+	this->chart = chart;
+}
+
+
+//choice ê°’ ìˆëŠ”ì§€ í™•ì¸, ëŒ€ì…
+int CheckChoice::SetValue(vector<int>dice_num)
+{
+	int result = 0;
+	result = accumulate(dice_num.begin(), dice_num.end(), 0);
+	return result;
+}
+CheckChoice::CheckChoice(Chart* chart)
+{
+	this->chart = chart;
+}
+
+//Full House ê°’ ìˆëŠ”ì§€ í™•ì¸, ëŒ€ì…
 int CheckFH::SetValue(vector<int>dice_num)
 {
 	int result = 0;
@@ -57,7 +141,7 @@ int CheckFH::SetValue(vector<int>dice_num)
 	return result;
 }
 
-//Small Straight °ª ÀÖ´ÂÁö È®ÀÎ, ´ëÀÔ
+//Small Straight ê°’ ìˆëŠ”ì§€ í™•ì¸, ëŒ€ì…
 int CheckSS::SetValue(vector<int>dice_num)
 {
 	int result = 0;
@@ -75,7 +159,7 @@ int CheckSS::SetValue(vector<int>dice_num)
 	return result;
 }
 
-//Big Straight °ª ÀÖ´ÂÁö È®ÀÎ, ´ëÀÔ
+//Big Straight ê°’ ìˆëŠ”ì§€ í™•ì¸, ëŒ€ì…
 int CheckBS::SetValue(vector<int>dice_num)
 {
 	int result = 0;
@@ -86,7 +170,7 @@ int CheckBS::SetValue(vector<int>dice_num)
 	return result;
 }
 
-//yacht °ª ÀÖ´ÂÁö È®ÀÎ, ´ëÀÔ
+//yacht ê°’ ìˆëŠ”ì§€ í™•ì¸, ëŒ€ì…
 int CheckYacht::SetValue(vector<int>dice_num)
 {
 	int result = 50;
@@ -95,7 +179,7 @@ int CheckYacht::SetValue(vector<int>dice_num)
 	else return 0;
 }
 
-//choice °ª ÀÖ´ÂÁö È®ÀÎ, ´ëÀÔ
+//choice ê°’ ìˆëŠ”ì§€ í™•ì¸, ëŒ€ì…
 int CheckChoice::SetValue(vector<int>dice_num)
 {
 	int result = 0;
